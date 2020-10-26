8f866964f608437f62f1dd2c01c09467017f5f88
Umar585 <techdeveloper585@gmail.com>
2020-10-26 11:17:07 -0400

    made slight changes

--- src/App.js
+++ src/App.js
@@ -3,25 +3,38 @@ import './app.css';
 
 function App() {
   return (
-    <div className="App">
-      <div class = "container">
-        <h1 class="display-2 center">Quiz App</h1>
-        <p class="lead center pl-5 mb-4">Get started with best quizes</p>
-      
+    <div className="container">
+      {/* header */}
+      <h1 className="display-4 text-center text-danger">Quiz App</h1>
+      {/* subtitle */}
+      <p className="lead text-center mb-4">Get started with best quiz's</p>
+      <div className="custom-file">
+        <form className="mt-4">
+          {/* custom file input */}
+          <label className="custom-file-label" htmlFor="customFile">Choose file</label>
+          <input type="file" className="custom-file-input" id="customFile" />
+          {/* Submit button */}
+          <div className="text-center">
+          <input type="submit" className="btn btn-danger mt-3" value="Add quiz" />
+          </div>
+        </form>
       </div>
 
-
-
-      <div class = "custom-file">
-        <input type = "file" class = "custom-file-input" id = "customFile"></input>
-        <label class ="custom-file-label" for="customFile">Choose file</label>
-        <button class = "btn btn-danger">Add quiz</button>
+      {/*make an example clickable box for pdfs to open */}
+      <div className="row" style={{ marginTop: '100px' }}>
+        <div className="col-sm-3" style={{marginTop: '10px'}}>
+          <div style={{
+            border: '1px solid black',
+            width: '100%',
+            height: '100%',
+            padding: '20px'
+          }}>
+            Clickable Example Text
+          </div>
+        </div>
       </div>
-
-      
-      {/*This is basically html. Add code in between the div */}
-      
     </div>
+
   );
 }
 
